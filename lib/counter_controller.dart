4e9a06139371ce38636b6afe4d3ff3724534cee8
DavaRamadhann <davaramadhan561@gmail.com>
2026-02-10 09:44:57 +0700

    task 1: terapkan SRP dan multi-step counter

---
+++ lib/counter_controller.dart
@@ -0,0 +1,25 @@
+class CounterController {
+  int _counter = 0;
+  int _step = 1; // default step
+
+  int get value => _counter;
+  int get step => _step;
+
+  void setStep(int newStep) {
+    if (newStep > 0) {
+      _step = newStep;
+    }
+  }
+
+  void increment() {
+    _counter += _step;
+  }
+
+  void decrement() {
+    _counter -= _step;
+  }
+
+  void reset() {
+    _counter = 0;
+  }
+}
---
+++ lib/counter_view.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'counter_controller.dart';
+
+class CounterView extends StatefulWidget {
+  const CounterView({super.key});
+
+  @override
+  State<CounterView> createState() => _CounterViewState();
+}
+
+class _CounterViewState extends State<CounterView> {
+  final CounterController _controller = CounterController();
+  final TextEditingController _stepController = TextEditingController(text: "1");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Task 1 - Multi-Step Counter")),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Nilai Counter:"),
+            Text(
+              '${_controller.value}',
+              style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+
+            const SizedBox(height: 16),
+
+            TextField(
+              controller: _stepController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: "Step",
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (val) {
+                final step = int.tryParse(val) ?? 1;
+                _controller.setStep(step);
+              },
+            ),
+
+            const SizedBox(height: 16),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () => setState(() => _controller.increment()),
+                  child: const Text("+"),
+                ),
+                ElevatedButton(
+                  onPressed: () => setState(() => _controller.decrement()),
+                  child: const Text("-"),
+                ),
+                ElevatedButton(
+                  onPressed: () => setState(() => _controller.reset()),
+                  child: const Text("Reset"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
